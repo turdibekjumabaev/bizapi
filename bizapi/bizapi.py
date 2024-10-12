fec32ffae154294f6a8b4326ad5aabd71733285d
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-12 11:47:17 +0500

    Exception Handler

--- bizapi/bizapi.py
+++ bizapi/bizapi.py
@@ -14,19 +14,20 @@ class BizAPI:
 
     def __init__(self, template_dir: str = 'templates', static_dir: str = 'static'):
         self.router = Router()
-        self.whitenoise = WhiteNoise(self.wsgi_app, root=static_dir)
+        self.whitenoise = WhiteNoise(self._wsgi_app, root=static_dir)
+        self.exception_handler = None
 
         globals.template_environment.loader = FileSystemLoader(os.path.abspath(template_dir))
 
     def __call__(self, environ, start_response):
         return self.whitenoise(environ, start_response)
 
-    def wsgi_app(self, environ, start_response):
+    def _wsgi_app(self, environ, start_response):
         request = Request(environ)
-        response = self.handle_request(request)
+        response = self._handle_request(request)
         return response(environ, start_response)
 
-    def handle_request(self, request):
+    def _handle_request(self, request):
         response = Response()
         handler_data, kwargs = self.router.find_handler(request)
         if handler_data is not None:
@@ -39,12 +40,24 @@ class BizAPI:
                 handler_method = getattr(handler, request.method.lower(), None)
                 if handler_method is None:
                     return method_not_allowed(response)
-                handler_method(request, response, **kwargs)
-            else:
+                handler = handler_method
+            try:
                 handler(request, response, **kwargs)
+            except Exception as e:
+                if self.exception_handler is not None:
+                    self.exception_handler(request, response, e)
+                else:
+                    raise e
         else:
             return page_not_found(response)
         return response
+
+    def _add_method_route(self, path: str, methods: list = None):
+        def wrapper(func):
+            self.router.add_route(path, func, methods)
+            return func
+
+        return wrapper
 
     def route(self, path: str, methods: list = None):
         if methods is None:
@@ -56,41 +69,37 @@ class BizAPI:
 
         return wrapper
 
-    def add_method_route(self, path: str, methods: list = None):
-        def wrapper(func):
-            self.router.add_route(path, func, methods)
-            return func
-
-        return wrapper
-
     def register_route(self, path: str, handler, methods: list = None):
         if methods is None:
             methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'TRACE', 'CONNECT']
         self.router.add_route(path, handler, methods)
 
+    def add_exception_handler(self, handler):
+        self.exception_handler = handler
+
     def get(self, path: str):
-        return self.add_method_route(path, ['GET'])
+        return self._add_method_route(path, ['GET'])
 
     def post(self, path: str):
-        return self.add_method_route(path, ['POST'])
+        return self._add_method_route(path, ['POST'])
 
     def put(self, path: str):
-        return self.add_method_route(path, ['PUT'])
+        return self._add_method_route(path, ['PUT'])
 
     def patch(self, path: str):
-        return self.add_method_route(path, ['PATCH'])
+        return self._add_method_route(path, ['PATCH'])
 
     def delete(self, path: str):
-        return self.add_method_route(path, ['DELETE'])
+        return self._add_method_route(path, ['DELETE'])
 
     def head(self, path: str):
-        return self.add_method_route(path, ['HEAD'])
+        return self._add_method_route(path, ['HEAD'])
 
     def options(self, path: str):
-        return self.add_method_route(path, ['OPTIONS'])
+        return self._add_method_route(path, ['OPTIONS'])
 
     def trace(self, path: str):
-        return self.add_method_route(path, ['TRACE'])
+        return self._add_method_route(path, ['TRACE'])
 
     def connect(self, path: str):
-        return self.add_method_route(path, ['CONNECT'])
+        return self._add_method_route(path, ['CONNECT'])
