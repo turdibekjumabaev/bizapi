353374c0a66516b083354ee7baa08b83906b8db1
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-10-20 09:23:19 +0500

    Update Response class

--- bizapi/types.py
+++ bizapi/types.py
@@ -4,19 +4,22 @@ import json
 
 
 class Response:
-    def __init__(self):
+    def __init__(self, body=b'', status=None, app_iter=None, content_type=None, conditional_response=None, **kw):
         self.json = None
         self.html = None
         self.text = None
-        self.content_type = None
-        self.body = b''
-        self.status_code = 200
+        self.content_type = content_type
+        self.body = body if isinstance(body, bytes) else body.encode('utf-8')
+        self.status_code = status or 200
+        self.app_iter = app_iter
+        self.conditional_response = conditional_response
+        self.kwargs = kw
 
     def __call__(self, environ, start_response):
         self.set_body_and_content_type()
 
         response = WebObResponse(
-            body=self.body, content_type=self.content_type, status=str(self.status_code)
+            body=self.body, content_type=self.content_type, status=str(self.status_code), app_iter=self.app_iter, conditional_response=self.conditional_response
         )
 
         return response(environ, start_response)
